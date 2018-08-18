c9990ae7e77edf819b12aa3f3fe1d1398f80148e
Vince Broz <vince@broz.cc>
2018-08-18 15:19:39 -0400

    Add type annotation

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -18,7 +18,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=6.0',{%- endif %}]
+requirements: List[str] = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=6.0',{%- endif %}]
 
 setup_requirements: List[str] = [{%- if cookiecutter.use_pytest == 'y' %}'pytest-runner',{%- endif %}]
 
