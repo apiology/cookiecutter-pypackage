0ea0a746898349b3d8d837a3ca383e8ff6ccae2d
Vince Broz <vince@broz.cc>
2021-01-30 20:51:27 -0500

    Install Ruby and overcommit

--- {{cookiecutter.project_slug}}/fix.sh
+++ {{cookiecutter.project_slug}}/fix.sh
@@ -2,6 +2,73 @@
 
 set -o pipefail
 
+install_rbenv() {
+  if [ "$(uname)" == "Darwin" ]
+  then
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install rbenv || true
+    if ! type rbenv 2>/dev/null
+    then
+      # https://github.com/pyenv/pyenv-installer/blob/master/bin/pyenv-installer
+      >&2 cat <<EOF
+WARNING: seems you still have not added 'rbenv' to the load path.
+
+# Load rbenv automatically by adding
+# the following to ~/.bashrc:
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+EOF
+    fi
+  else
+    git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  fi
+}
+
+set_rbenv_env_variables() {
+  export PATH="${HOME}/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+}
+
+install_ruby_build() {
+  if [ "$(uname)" == "Darwin" ]
+  then
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install ruby-build || true
+  else
+    mkdir -p "$(rbenv root)"/plugins
+    git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+  fi
+}
+
+ensure_ruby_build() {
+  if ! type ruby-build >/dev/null 2>&1 && ! [ -d "${HOME}/.rbenv/plugins/ruby-build" ]
+  then
+    install_ruby_build
+  fi
+}
+
+ensure_rbenv() {
+  if ! type rbenv >/dev/null 2>&1 && ! [ -f "${HOME}/.rbenv/bin/rbenv" ]
+  then
+    install_rbenv
+  fi
+
+  if ! type rbenv >/dev/null 2>&1
+  then
+    set_rbenv_env_variables
+  fi
+
+  ensure_ruby_build
+}
+
+ensure_ruby_version() {
+  rbenv install -s "$(cat .ruby-version)"
+}
+
+ensure_bundle() {
+  bundle --version >/dev/null 2>&1 || gem install bundler
+  bundle install
+}
+
 latest_version() {
   major_minor=${1}
   # https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
@@ -45,11 +112,22 @@ do
   fi
 done
 
-# assumes you have an existing pyenv named mylibs where your global
-# stuff goes
+ensure_rbenv
+
+ensure_ruby_version
+
+ensure_bundle
+
+ensure_pyenv
+
+ensure_python_versions
+
+
 latest_python_version="$(cut -d' ' -f1 <<< "${python_versions}")"
 virtualenv_name="{{ cookiecutter.project_slug }}-${latest_python_version}"
 pyenv virtualenv "${latest_python_version}" "${virtualenv_name}" || true
+# assumes you have an existing pyenv named mylibs where your global
+# stuff goes
 pyenv local "${virtualenv_name}" ${python_versions} mylibs
 # Make sure we have a pip with the 20.3 resolver, and after the
 # initial bugfix release
