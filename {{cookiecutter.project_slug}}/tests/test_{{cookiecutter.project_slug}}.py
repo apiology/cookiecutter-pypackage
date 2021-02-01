2ee5c7506533651eb370cf19e73f230d9d7b4929
Vince Broz <vince@broz.cc>
2021-02-01 10:54:49 -0500

    Fix templating error

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -57,7 +57,7 @@ optional arguments:
 """
     # older python versions show arguments like this:
     alt_expected_help = expected_help.replace('[_ ...]', '[_ [_ ...]]')
-    actual_help = subprocess.check_output(['{{ cookiecutter.project_slug }', '--help']).decode('utf-8')
+    actual_help = subprocess.check_output(['{{ cookiecutter.project_slug }}', '--help']).decode('utf-8')
     assert actual_help in [expected_help, alt_expected_help]
 {%- endif %}
 {%- else %}
