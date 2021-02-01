85815416a4327dfb13dca9e487ac47b5019f9676
Vince Broz <vince@broz.cc>
2021-02-01 12:02:29 -0500

    I learned something today about jinja

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -33,9 +33,9 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
+
+
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
@@ -46,6 +46,8 @@ def test_command_line_interface():
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
 {%- else %}
+
+
 def test_cli_help():
     expected_help = """usage: {{ cookiecutter.project_slug }} [-h] [_ ...]
 
