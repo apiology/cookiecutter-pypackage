024796d7ddb9edf91c2bf3e70584b9ff1cbd46b5
Vince Broz <vince@broz.cc>
2021-02-01 10:31:09 -0500

    Add example argparse test

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -33,9 +33,9 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
-
-
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
@@ -45,6 +45,18 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+{%- else %}
+def test_cli_help():
+    expected_help = b"""usage: op-env [-h] [_ ...]
+
+positional arguments:
+  _
+
+optional arguments:
+  -h, --help  show this help message and exit
+"""
+    actual_help = subprocess.check_output(['op-env', '--help'])
+    assert expected_help == actual_help
 {%- endif %}
 {%- else %}
 
